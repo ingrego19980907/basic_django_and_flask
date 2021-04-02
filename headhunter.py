975226f5ab7feda37cab46bb156195d9d8ee0d12
Viatcheslav_Suhodoiev <>
2021-04-02 20:09:23 +0000

    find name company and job

--- headhunter.py
+++ headhunter.py
@@ -28,6 +28,15 @@ def extract_max_page():
   return pages[-1]
 
 def extract_hh_jobs(last_page):
-  for page in range(last_page):
-    result = requests.get(f'{URL}&page={page}', headers=HEADERS)
-    print(result.status_code)
+  jobs = []
+  # for page in range(last_page):
+  result = requests.get(f'{URL}&page=0', headers=HEADERS)
+  print(result.status_code)
+  soup = BeautifulSoup(result.text, 'html.parser')
+  results = soup.find_all('div', {'class': 'vacancy-serp-item'})
+  for result in results:
+    title = result.find('a').text
+    company = result.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).find('a').text
+    print(company)
+
+  return jobs
